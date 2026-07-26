bd84c48761209cd458748b0930d83c32ceed342e
Varun Mathur <varunmathur817@gmail.com>
2026-07-26 18:29:24 +0530

    backend

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+// import dns from "dns";
+
+// dns.setServers([
+//   "8.8.8.8",
+//   "8.8.4.4"
+// ]);
+
+// import express from "express";
+// import dotenv from "dotenv";
+
+// import connectDB from "./config/db.js";
+// import router from "./routes/contact.routes.js";
+
+// dotenv.config();
+
+// const app = express();
+
+// console.log(process.env.MONGODB_URI);
+
+// connectDB();
+
+// app.use(express.json());
+
+// app.use("/api", router);
+
+// const PORT = process.env.PORT || 3001;
+
+// app.listen(PORT, () => {
+//   console.log(`Server Running On Port ${PORT}`);
+// });
+
 import dns from "dns";
 
 dns.setServers([
@@ -5,26 +36,59 @@ dns.setServers([
   "8.8.4.4"
 ]);
 
+
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import connectDB from "./config/db.js";
 import router from "./routes/contact.routes.js";
 
+
 dotenv.config();
+
 
 const app = express();
 
-console.log(process.env.MONGODB_URI);
 
-connectDB();
+// CORS
+app.use(
+  cors({
+  origin:[
+ "http://localhost:3000",
+ "http://127.0.0.1:3000"
+],
+    methods: [
+      "GET",
+      "POST",
+      "PUT",
+      "DELETE"
+    ],
+    credentials: true
+  })
+);
+
+
 
 app.use(express.json());
 
-app.use("/api", router);
+
+connectDB();
+
+
+app.use(
+  "/api",
+  router
+);
+
 
 const PORT = process.env.PORT || 3001;
 
+
 app.listen(PORT, () => {
-  console.log(`Server Running On Port ${PORT}`);
+
+  console.log(
+    `Server Running On Port ${PORT}`
+  );
+
 });
--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,9 +1,25 @@
 import express from "express";
 import { contactForm, getContacts } from "../controller/contact.controller.js";
+import { getHomeProducts, homeproductcontroller } from "../controller/home.controller.js";
 
 const router = express.Router();
 
 router.post("/contact", contactForm);
 router.get("/getcontacts", getContacts);
+
+
+
+// homae page product
+router.post(
+    "/home-page-product",
+    homeproductcontroller
+);
+router.get(
+    "/home-page-product",
+    getHomeProducts
+);
+
+
+
 
 export default router;
